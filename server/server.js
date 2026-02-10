272a6ed795ae9f4de68e1f744e20e47b6e5c3035
Neerajkr-jha <neeraj.23b1541133@abes.ac.in>
2026-02-10 22:11:59 +0530

    deployment changes

--- server/server.js
+++ server/server.js
@@ -5,23 +5,41 @@ import connectDb from "./configs/db.js";
 import userRouter from "./routes/userRoutes.js";
 import resumeRouter from "./routes/resumeRoutes.js";
 import aiRouter from "./routes/aiRouter.js";
+//import { connect } from "mongoose";
 
 
 const app=express();
-const PORT=process.env.PORT || 3000;
+// const PORT=process.env.PORT || 3000;
 
 //database connnection 
-await connectDb()
+// await connectDb()
 
 //middleware
 app.use(express.json())
 app.use(cors())
+
+let isConnected=false;
+
+async function connectOnce() {
+    if(!isConnected){
+        await connectDb();
+        isConnected=true
+        console.log("Databse connected")
+    }
+}
+
+app.use(async(req,res,next)=>{
+    await connectOnce();
+    next();
+})
 
 app.get("/",(req,res)=>res.send("Server is Live..."));
 app.use("/api/users", userRouter)
 app.use("/api/resumes",resumeRouter)
 app.use("/api/ai",aiRouter)
 
-app.listen(PORT,()=>{
-    console.log(`server is running at PORT ${PORT}`);
-})
+// app.listen(PORT,()=>{
+//     console.log(`server is running at PORT ${PORT}`);
+// })
+
+export default app
