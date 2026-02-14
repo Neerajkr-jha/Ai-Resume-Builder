02c12451cfffbe60911f805f0f1d1db45dec5513
Neerajkr-jha <neeraj.23b1541133@abes.ac.in>
2026-02-14 07:24:06 +0530

    authslice

--- frontend/src/app/features/authSlice.js
+++ frontend/src/app/features/authSlice.js
@@ -1,21 +1,28 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+const token = localStorage.getItem("token");
+const user = localStorage.getItem("user");
+
 const authSlice=createSlice({
     name:'auth',
     initialState:{
-        token:null,
-        user:null,
-        loading:true
+        token:token || null,
+        user:user ? JSON.parse(user) :null ,
+        loading:false
     },
     reducers:{
         login:(state,action)=>{
             state.token=action.payload.token
             state.user=action.payload.user
+
+            localStorage.setItem("token", action.payload.token);
+            localStorage.setItem("user", JSON.stringify(action.payload.user));
         },
         logout:(state)=>{
             state.token='',
             state.user=null,
             localStorage.removeItem('token')
+            localStorage.removeItem('user')
         },
         setLoading:(state,action)=>{
             state.loading=action.payload
