abc0b2632766054c790f64dec431fde884809dbb
Neerajkr-jha <jhaneeraj290@gmail.com>
2026-04-01 09:46:20 +0530

    res code

--- server/controlers/userController.js
+++ server/controlers/userController.js
@@ -68,7 +68,7 @@ export const loginUser = async (req, res) => {
 
         return res.status(200).json({ message: "Login sucessfull", token, user });
     } catch (error) {
-        return res.status(400).json({ message: error.message })
+        return res.status(500).json({ message: error.message })
     }
 }
 
